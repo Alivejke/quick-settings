e8c37d59cf2a6efa8cec633fe0d0d02a7f524c0d
Alivejke <alivejke@gmail.com>
2014-03-21 23:01:53 +0200

    navigation

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,26 @@
 'use strict';
 
-angular.module('app.controllers', ['app.services'])
-    .controller('QuickSettingsCtrl', ['$scope', 'quickSettings', function($scope, quickSettings) {
+angular.module('app.controllers', ['app.services', 'ngAnimate'])
+    .controller('QuickSettingsCtrl', ['$scope', 'quickSettings', '$timeout', function($scope, quickSettings, $timeout) {
         $scope.avaliableSettings = quickSettings.avaliableSettings;
         $scope.activeSettings = quickSettings.activeSettings;
+
+        $scope.currentTime = (new Date).getTime();
+
+        $scope.settingsSlideUp = function() {
+            var popped = $scope.avaliableSettings.pop();
+
+            $timeout(function() {
+                $scope.avaliableSettings.unshift(popped);
+            }, 0);
+        };
+
+        $scope.settingsSlideDown = function() {
+            var shifted = $scope.avaliableSettings.shift();
+
+            $timeout(function() {
+                $scope.avaliableSettings.push(shifted);
+            }, 0);
+        };
+
     }]);
--- app/js/services.js
+++ app/js/services.js
@@ -3,11 +3,39 @@
 angular.module('app.services', ['LocalStorageModule'])
     .factory('quickSettings', ['localStorageService', function(lsService) {
         var factory = {},
+            callSign = 'WSCBS',
+            channelNumber = 2,
             activeSettings = lsService.get('quickSettings'),
-            avaliableSettings = {
-                language: ['English', 'Español', 'Français'],
-                sleepTimer: ['OFF', '5', '15', '30', '45', '60', '75', '90', '105', '120', '180', '240']
-            };
+            avaliableSettings = [
+                {
+                    name: 'recording',
+                    textEnabled: 'Stop recording ' + callSign + ' ' + channelNumber + ' now',
+                    textDisabled:'Start recording ' + callSign + ' ' + channelNumber + ' now'
+                },
+                {
+                    name: 'favoriteChannel',
+                    textEnabled: 'Remove ' + callSign + ' ' + channelNumber + ' from Favorites List',
+                    textDisabled:'Add ' + callSign + ' ' + channelNumber + ' to Favorites List'
+                },
+                {
+                    name: 'language',
+                    text: 'Choose SAP language',
+                    items: ['English', 'Español', 'Français'],
+                    type: 'slider'
+                },
+                {
+                    name: 'sleepTimer',
+                    text: 'Set Sleep Timer  (minutes)',
+                    items: ['OFF', 5, 15, 30, 45, 60, 75, 90, 105, 120, 180, 240],
+                    type: 'slider',
+                    showCurrent: true
+                },
+                {
+                    name: 'parentalControl',
+                    textEnabled: 'Turn OFF Parental Control',
+                    textDisabled: 'Turn ON Parental Control'
+                }
+            ];
 
         factory.setSettings = function(settings) {
             lsService.set('quickSettings', settings);
